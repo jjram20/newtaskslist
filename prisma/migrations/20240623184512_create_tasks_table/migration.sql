ca061b33956b220b7377a1203ea1b5e47f6f30e1
jjram20 <jde.ram9@gmail.com>
2024-06-23 12:48:56 -0600

    Table for tasks created

---
+++ prisma/migrations/20240623184512_create_tasks_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "content" TEXT NOT NULL,
+    "dateline" DATETIME NOT NULL,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
